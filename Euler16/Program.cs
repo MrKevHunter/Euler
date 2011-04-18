f5aced3e84c8d0d3d0a34c5cee625c24348db37d
Kev Hunter <MrKevHunter@gmail.com>
2011-04-18 21:37:17 +0100

    Added E16

---
+++ Euler16/Program.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+using System;
+using System.Linq;
+using Swensen;
+
+namespace Euler16
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            BigInt bigInt = BigInt.Pow(2, 1000);
+            int result = bigInt.ToString().Sum(l => Int32.Parse(l.ToString()));
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
