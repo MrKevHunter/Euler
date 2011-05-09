8a27f2d4ac6c59eee50a36a49fe2649bfb296bc3
Kev Hunter <MrKevHunter@gmail.com>
2011-05-09 22:19:09 +0100

    E25

---
+++ Euler25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Swensen;
+
+namespace Euler25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInt lhs = 0;
+            BigInt rhs = 1;
+            BigInt total = 0;
+            int counter = 1;
+            while (true)
+            {
+                counter++;
+                total = lhs + rhs;
+                lhs = rhs;
+                rhs = total;
+                Console.WriteLine(total);
+                if (total.ToString().Length >= 1000 )
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(counter);
+            Console.ReadLine();
+        }
+    }
+}
