8206bb4415c3ad2c8a688761c2057c929c558be7
Kev Hunter <MrKevHunter@gmail.com>
2011-05-25 20:54:56 +0100

    added euler 9

--- Euler 4/Program.cs
+++ Euler 4/Program.cs
@@ -1,46 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 
 namespace Euler_4
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-           IEnumerable<int> palindromes =  GetPalindromes(998001);
-      
-            Console.WriteLine("{0} palindromes found",palindromes.Count());
-            foreach (var palindrome in palindromes)
+            var s = new Stopwatch();
+            s.Start();
+            IEnumerable<int> palindromes = GetPalindromes(998001);
+
+            Console.WriteLine("{0} palindromes found", palindromes.Count());
+            foreach (int palindrome in palindromes)
             {
                 for (int i = 999 - 1; i >= 0; i--)
                 {
                     for (int j = 999 - 1; j >= 0; j--)
                     {
                         int result = i*j;
-                        if (result<palindrome)
+                        if (result < palindrome)
                         {
                             break;
                         }
                         if (result == palindrome)
                         {
-                            Console.WriteLine("{0} * {1} = {2}",i,j,palindrome);
+                            Console.WriteLine("{0} * {1} = {2}", i, j, palindrome);
+                            s.Stop();
+                            Console.WriteLine("Found in {0} milliseconds", s.ElapsedMilliseconds);
                             Console.ReadLine();
+                            return;
                             break;
                         }
                     }
                 }
             }
-            Console.ReadLine();
         }
 
         private static IEnumerable<int> GetPalindromes(int max)
         {
             for (int i = max; i >= 0; i--)
             {
-
-                if (i.ToString()==new string(i.ToString().Reverse().ToArray()))
+                if (i.ToString() == new string(i.ToString().Reverse().ToArray()))
                 {
                     yield return i;
                 }
---
+++ Euler9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Euler9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            foreach (var i in Enumerable.Range(1,1000))
+            {
+                foreach (var j in Enumerable.Range(1,1000))
+                {
+                    var total = 1000 - i - j;
+                    if (i * i + j * j == total * total)
+                    {
+                        result = i * j * total;
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
