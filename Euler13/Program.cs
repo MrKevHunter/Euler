1a9027a915e0ed6555831375c0b6cde96513cb9c
Kev Hunter <MrKevHunter@gmail.com>
2011-05-26 19:44:39 +0100

    Euler 13 - DONE

---
+++ Euler13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Swensen;
+
+namespace Euler13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] readAllLines = File.ReadAllLines("numbers.txt");
+            BigInt total = readAllLines.Aggregate<string, BigInt>(0, (current, readAllLine) => current + (BigInt) readAllLine);
+            Console.WriteLine(total);
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine(total.ToString().Substring(0,10));
+            Console.ReadLine();
+        }
+    }
+}
